ff7afb3781b493fef078f5e99237e058d683bf75
panyapol-Cry9 <panyapolboomm@gmail.com>
2026-07-24 08:57:19 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Hello my name is ");
+        String A = input.next();
+        System.out.print("My nickname is ");
+        String B = input.next();
+        }
+    }
